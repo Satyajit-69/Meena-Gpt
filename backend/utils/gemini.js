d75bc7169236389f3157e91d3d24385681f34efc
Satya Satyajit <satyajitsahoo28252@gmail.com>
2025-12-15 17:02:09 +0530

    changing the model again

--- backend/utils/gemini.js
+++ backend/utils/gemini.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import "dotenv/config";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const getGeminiResponse = async (message, useChainOfThought = true) => {
   try {
